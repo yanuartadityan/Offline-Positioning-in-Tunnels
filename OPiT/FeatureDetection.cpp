099198b0abbba3f875fff9810e21d0f6aaf7f77a
Janu Aditya <yanuart.adityan@gmail.com>
2016-04-19 10:46:19 +0200

    last before branch

--- OPiT/FeatureDetection.cpp
+++ OPiT/FeatureDetection.cpp
@@ -35,42 +35,42 @@ void FeatureDetection::computeKeypointsAndDraw(char *pathname)
     char imagenameentr[100];
     char imagenameearl[100];
     char imagenameinsi[100];
-    
+
     sprintf(imagenameentr, "%simg_%05d.png", pathname, IMG_ENTRANCE);
     sprintf(imagenameearl, "%simg_%05d.png", pathname, IMG_EARLY);
     sprintf(imagenameinsi, "%simg_%05d.png", pathname, IMG_INSIDE);
-    
+
     Mat img0 = imread(imagenameentr);
     Mat img1 = imread(imagenameearl);
     Mat img2 = imread(imagenameinsi);
-    
+
     // work with grayscale only
     cvtColor(img0, img0, COLOR_BGR2GRAY);
-    
+
     vector<KeyPoint> fastpoints, surfpoints, siftpoints;
     vector<KeyPoint> fastpoints1, surfpoints1, siftpoints1;
     vector<KeyPoint> fastpoints2, surfpoints2, siftpoints2;
-    
+
     // fast detection
     fastdetect_->detect(img0, fastpoints);
     fastdetect_->detect(img1, fastpoints1);
     fastdetect_->detect(img2, fastpoints2);
-    
+
     cout << "FAST: " << endl;
     cout << "entra keypoints: " << fastpoints.size() << endl;
     cout << "early keypoints: " << fastpoints1.size() << endl;
     cout << "insid keypoints: " << fastpoints2.size() << endl;
-    
+
     // surf detection
     surfdetect_->detect(img0, surfpoints);
     surfdetect_->detect(img1, surfpoints1);
     surfdetect_->detect(img2, surfpoints2);
-    
+
     cout << "SURF: " << endl;
     cout << "entra keypoints: " << surfpoints.size() << endl;
     cout << "early keypoints: " << surfpoints1.size() << endl;
     cout << "insid keypoints: " << surfpoints2.size() << endl;
-    
+
     // sift detection
     siftdetect_->detect(img0, siftpoints);
     siftdetect_->detect(img1, siftpoints1);
@@ -80,24 +80,24 @@ void FeatureDetection::computeKeypointsAndDraw(char *pathname)
     cout << "entra keypoints: " << siftpoints.size() << endl;
     cout << "early keypoints: " << siftpoints1.size() << endl;
     cout << "insid keypoints: " << siftpoints2.size() << endl;
-    
+
     // draw
     namedWindow("fast-entrance", WINDOW_NORMAL);
     namedWindow("fast-early", WINDOW_NORMAL);
     namedWindow("fast-inside", WINDOW_NORMAL);
-    
+
     namedWindow("surf-entrance", WINDOW_NORMAL);
     namedWindow("surf-early", WINDOW_NORMAL);
     namedWindow("surf-inside", WINDOW_NORMAL);
-    
+
     namedWindow("sift-entrance", WINDOW_NORMAL);
     namedWindow("sift-early", WINDOW_NORMAL);
     namedWindow("sift-inside", WINDOW_NORMAL);
-    
+
     Mat fast0, fast1, fast2;
     Mat surf0, surf1, surf2;
     Mat sift0, sift1, sift2;
-    
+
     drawKeypoints(img0, fastpoints,  fast0);
     drawKeypoints(img1, fastpoints1, fast1);
     drawKeypoints(img2, fastpoints2, fast2);
@@ -105,15 +105,15 @@ void FeatureDetection::computeKeypointsAndDraw(char *pathname)
     drawKeypoints(img0, surfpoints, surf0);
     drawKeypoints(img1, surfpoints1, surf1);
     drawKeypoints(img2, surfpoints2, surf2);
-    
+
     drawKeypoints(img0, siftpoints, sift0);
     drawKeypoints(img1, siftpoints1, sift1);
     drawKeypoints(img2, siftpoints2, sift2);
-    
+
     imshow("fast-entrance", fast0);
     imshow("fast-early", fast1);
     imshow("fast-inside", fast2);
-    
+
     imshow("surf-entrance", surf0);
     imshow("surf-early", surf1);
     imshow("surf-inside", surf2);
@@ -121,24 +121,24 @@ void FeatureDetection::computeKeypointsAndDraw(char *pathname)
     imshow("sift-entrance", sift0);
     imshow("sift-early", sift1);
     imshow("sift-inside", sift2);
-    
+
     // write to file
     vector<int> compression_params;
     compression_params.push_back(IMWRITE_PNG_COMPRESSION);
     compression_params.push_back(9);
-    
+
     imwrite("fast-entrance.png", fast0, compression_params);
     imwrite("fast-early.png",    fast1, compression_params);
     imwrite("fast-inside.png",   fast2, compression_params);
-    
+
     imwrite("surf-entrance.png", surf0, compression_params);
     imwrite("surf-early.png",    surf1, compression_params);
     imwrite("surf-inside.png",   surf2, compression_params);
-    
+
     imwrite("sift-entrance.png", sift0, compression_params);
     imwrite("sift-early.png",    sift1, compression_params);
     imwrite("sift-inside.png",   sift2, compression_params);
-    
+
     cv::waitKey(1);
 }
 
@@ -176,7 +176,7 @@ void FeatureDetection::drawKeypoints (cv::Mat img, std::vector<cv::KeyPoint> det
 {
     // copy original image
     img.copyTo(output);
-    
+
     // draw the keypoints using rich method (draw the radius)
     cv::drawKeypoints(img, detectedPoints, output, Scalar(249, 205, 47), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 }
