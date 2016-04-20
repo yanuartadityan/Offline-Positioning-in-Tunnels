eaefe0467b4f6b51af42dd90913a33d61f3908de
Janu Aditya <yanuart.adityan@gmail.com>
2016-04-20 07:15:39 +0200

    add new dirs to ignore

---
+++ OPiT/buildunix/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/Calibration.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/Calibration.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/Converter.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/Converter.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/FeatureDetection.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/FeatureDetection.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/PCLTest.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/PCLTest.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/PnPSolver.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/PnPSolver.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/PointProjection.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/PointProjection.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/Reprojection.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/Reprojection.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/Triangulation.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/Triangulation.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/VisualOdometry.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/VisualOdometry.cpp.o"
+  "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/pclWrapper.cpp" "/Users/januaditya/Thesis/exjobb-data/git/Offline-Positioning-in-Tunnels/OPiT/buildunix/CMakeFiles/src.dir/pclWrapper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL)"
+  "vtkRenderingFreeType_AUTOINIT=1(vtkRenderingFreeTypeFontConfig)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv3/3.1.0_1/include/opencv"
+  "/usr/local/Cellar/opencv3/3.1.0_1/include"
+  "/usr/local/Cellar/vtk/6.3.0_1/include/vtk-6.3"
+  "/usr/local/include"
+  "/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7"
+  "/usr/include/libxml2"
+  "/usr/local/include/pcl-1.8"
+  "/usr/local/Cellar/eigen/3.2.8/include/eigen3"
+  "/usr/local/Cellar/openni/1.5.7.10/include/ni"
+  "/usr/local/include/ni2"
+  "/usr/local/Cellar/flann/1.8.4_1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
