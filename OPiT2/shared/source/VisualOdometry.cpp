274da26a156ac4cdfec8b28cc11687d99f5e7b48
Janu Aditya <yanuart.adityan@gmail.com>
2016-05-09 15:41:54 +0200

    vo test added

--- OPiT2/shared/source/VisualOdometry.cpp
+++ OPiT2/shared/source/VisualOdometry.cpp
@@ -14,7 +14,7 @@ using namespace std;
 using namespace cv;
 using namespace cv::xfeatures2d;
 
-#define STARTFRAME  1
+#define STARTFRAME  433
 #define MAXFRAME    2000
 
 /*
@@ -446,7 +446,7 @@ void VO::initParameter()
 
     /* 0 - fast,  1 - sift, 2 - surf,    3 - lukaskanade
        4 - akaze, 5 - orb,  6 - fastsift */
-    vo_method = 1;
+    vo_method = 0;
 
     // fast
     fast_threshold = 20;
--- OPiT2/tests-vo/Test-vo.cpp
+++ OPiT2/tests-vo/Test-vo.cpp
@@ -6,7 +6,14 @@ using namespace std;
 
 int main ()
 {
+    #ifndef KITTI_DATASET
+       char pathname[100] = "/Users/januaditya/Thesis/exjobb-data/volvo/out0/";
+    #else
+       char pathname[100] = "/Users/januaditya/Thesis/exjobb-data/kitti-odometry/dataset/sequences/00/image_0/";
+    #endif
+
     // VISUAL ODOMETRY
+    VO vodometry;
     vodometry.initParameter();
     vodometry.setImagePath(pathname);
     vodometry.visualodometry();
