c06d19074fb7a1e2d82f7d4c116742f673237ff8
Klumpen <david.bennehag@gmail.com>
2016-05-12 14:58:39 +0200

    Fixed and finalized merge, you're welcome.

--- OPiT/Reprojection.h
+++ OPiT/Reprojection.h
@@ -13,7 +13,6 @@ public:
 	cv::Mat foo(cv::Mat,cv::Mat, cv::Mat, cv::Mat, cv::Mat, cv::Mat);
 
 
-	static std::vector<double> backprojectRadius(cv::Mat T, cv::Mat K, cv::Point2d imagepoint, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
 	static std::vector<double> backproject(cv::Mat T, cv::Mat K, cv::Point2d imagepoint, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
     static std::vector<double> backprojectRadius(cv::Mat T, cv::Mat	K, cv::Point2d imagepoint, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
     static std::vector<double> LinearInterpolation(std::vector<double> bestPoint, cv::Mat origin, cv::Mat vectorPoint);
